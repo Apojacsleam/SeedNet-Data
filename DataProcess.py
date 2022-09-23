572a01712babfbf02c17b4f2164d64bd0aea2425
Apojacsleam <79567165+Apojacsleam@users.noreply.github.com>
2022-09-23 20:11:23 +0800

    Add files via upload

---
+++ DataProcess.py
@@ -0,0 +1,22 @@
+import numpy as np
+import networkx as nx
+
+npz_file = np.load('./Data/iSTNs_Years_ComCodes.npz')
+Years, Hscodes, NotEconomy = npz_file['Years'], npz_file['Hscodes'], npz_file['NotEconomy']
+
+
+def GetGraph(Hscode, years):
+    Graph = nx.read_gpickle(f'./Data/iSeedTradeNetwork_{Hscode}_{year}.gpickle')
+    GraphNodes = Graph.nodes()
+    RemoveNodes = []
+    # Remove non-economy nodes
+    for NonEcon in NotEconomy:
+        if NonEcon in GraphNodes:
+            RemoveNodes.append(NonEcon)
+    # Remove invalid nodes without trade links
+    for Gnode in GraphNodes:
+        if nx.degree(Graph, nbunch=Gnode) == 0:
+            RemoveNodes.append(Gnode)
+    for Gnode in RemoveNodes:
+        Graph.remove_node(Gnode)
+    return Graph
